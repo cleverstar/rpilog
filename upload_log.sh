38c1fefc93697bf3ae77d20b257b077e620045c8
Jianwei Li <jianweili@gmail.com>
2016-12-31 03:10:50 -0500

    Updated boot.log.rpi0

--- upload_log.sh
+++ upload_log.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 git pull
 update_file=boot.log.$(hostname)
 cat /var/log/boot.log > $update_file
-myip.sh >> $uplodate_file
+myip.sh >> $update_file
 git commit -am "Updated $update_file"
 git push origin
