4423e71453d61c15101c30a9b74e02ea6479b6a6
Jianwei Li <jianweili@gmail.com>
2016-12-31 04:12:24 -0500

    Updated boot.log.rpi0

--- upload_log.sh
+++ upload_log.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 git pull
 update_file=boot.log.$(hostname)
 sed -e 's/.\[32m//' -e 's/.\[0m//' /var/log/boot.log > $update_file
-#myip.sh >> $update_file
+myip.sh >> $update_file
 git commit -am "Updated $update_file"
 git push origin
