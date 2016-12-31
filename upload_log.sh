d1eff37af40be06fa2449439d1cb192dbd6fed78
Jianwei Li <jianweili@gmail.com>
2016-12-31 15:18:25 -0500

    Updated boot.log.rpi0

--- upload_log.sh
+++ upload_log.sh
@@ -8,7 +8,8 @@ done
 cd $(dirname $0)
 git pull
 update_file=boot.log.$(hostname)
-sed -e 's/.\[32m//' -e 's/.\[0m//' /var/log/boot.log > $update_file
-echo "My IP address is $myip" >> $update_file
+date > $update_file
+sed -e 's/.\[32m//' -e 's/.\[0m//' /var/log/boot.log >> $update_file
+echo "Host $(hostname) IP address $myip" >> $update_file
 git commit -am "Updated $update_file"
 git push origin
