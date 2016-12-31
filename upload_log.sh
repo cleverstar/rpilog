fd5d9f4f1a238f4678274203c5cf8111cab98cc2
Jianwei Li <jianweili@gmail.com>
2016-12-31 11:16:12 -0500

    Updated boot.log.rpi0

--- upload_log.sh
+++ upload_log.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+for ((i=0; i<10; i++)); do
+    if [ myip=$(ifconfig wlan0 | grep -oP "inet addr:\K\S+") ]; then
+        break;
+    else
+        sleep 1
+    fi
+done
+    
 cd $(dirname $0)
 git pull
 update_file=boot.log.$(hostname)
 sed -e 's/.\[32m//' -e 's/.\[0m//' /var/log/boot.log > $update_file
-myip.sh >> $update_file
+echo "My IP address is $myip" >> $update_file
 git commit -am "Updated $update_file"
 git push origin
